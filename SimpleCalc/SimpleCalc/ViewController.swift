1721ad432e7cebed126e5f517b52aa5c683f5547
Emil Berger <beremm14@htlkaindorf.at>
2019-04-28 22:04:18 +0200

    Add: GUI

---
+++ SimpleCalc/SimpleCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalc
+//
+//  Created by Emil Berger on 28.04.19.
+//  Copyright © 2019 Emil Berger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
